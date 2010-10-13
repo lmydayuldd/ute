efa81182c5c1fc5679e6a202510506090b8fb85f
Alexander Remesch <alexander.remesch@gmail.com>
2010-10-13 21:11:07 +0000

    Initial residential satisfaction calculation based on UDP diversity and public transport access

---
+++ src/at/sume/dm/types/HouseholdType.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package at.sume.dm.types;
+
+/**
+ * @author Alexander Remesch
+ *
+ */
+public enum HouseholdType {
+	SINGLE_YOUNG,
+	COUPLE_YOUNG,
+	SINGLE_PARENT,
+	SMALL_FAMILY,
+	LARGE_FAMILY,
+	SINGLE_OLD,
+	COUPLE_OLD,
+	OTHER;
+	
+	public static int getId(HouseholdType householdType) {
+		switch (householdType) {
+		case SINGLE_YOUNG:
+			return 1;
+		case COUPLE_YOUNG:
+			return 2;
+		case SINGLE_PARENT:
+			return 3;
+		case SMALL_FAMILY:
+			return 4;
+		case LARGE_FAMILY:
+			return 5;
+		case SINGLE_OLD:
+			return 6;
+		case COUPLE_OLD:
+			return 7;
+		case OTHER:
+			return 8;
+		default:
+			throw new AssertionError("Unknown householdType");
+		}
+	}
+}
