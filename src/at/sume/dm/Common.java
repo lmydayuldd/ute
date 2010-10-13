d265f3ec3dbe939054bc85a9575bb332f3a2da9e
Alexander Remesch <alexander.remesch@gmail.com>
2010-10-13 21:50:42 +0000

    Preparation for scenarios, changes in getsysparam

--- src/at/sume/dm/Common.java
+++ src/at/sume/dm/Common.java
@@ -18,6 +18,7 @@ import net.remesch.util.Database;
 public class Common {
 	public final static String INI_FILENAME = "sume_dm.ini";
 	public static Database db;
+	public static short scenarioId;
 	
 	/**
 	 * Get the location of the database from the INI-file
@@ -40,19 +41,25 @@ public class Common {
 		return(db);
 	}
 
+	public static void init() {
+		scenarioId = Short.parseShort(getSysParam("DefaultScenario"));
+	}
+	
 	/**
 	 * Retrieve the value of a system parameter (from the table Systemparameter)
 	 * @param paramName
 	 * @return
 	 */
 	public static String getSysParam(String paramName) {
-		String rv = null;
+		Object rv = null;
 		try {
-			rv = (String)db.lookupSql("select wert from SysParamsRuntime where name='" + paramName + "'");
+			rv = db.lookupSql("select wert from SysParamsRuntime where name='" + paramName + "'");
+			if (rv != null)
+				return (String)rv;
 		} catch (SQLException e) {
 			System.err.println("getSysParam: error looking up parameter " + paramName);
 			e.printStackTrace();
 		}
-		return(rv);
+		return null;
 	}
 }
