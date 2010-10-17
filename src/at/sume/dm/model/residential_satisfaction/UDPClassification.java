800fb225c7b3c66582fc3bd2d3e04fc3baac7800
Alexander Remesch <alexander.remesch@gmail.com>
2010-10-17 18:51:34 +0000

    HouseholdPrefs and SpatialUnitUdp now again implemented as nested inner classes here

--- src/at/sume/dm/model/residential_satisfaction/UDPClassification.java
+++ src/at/sume/dm/model/residential_satisfaction/UDPClassification.java
@@ -6,20 +6,279 @@ package at.sume.dm.model.residential_satisfaction;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
+
 import at.sume.dm.Common;
 import at.sume.dm.entities.HouseholdRow;
 import at.sume.dm.entities.SpatialUnitRow;
-import at.sume.dm.model.residential_satisfaction.entities.HouseholdPrefs;
-import at.sume.dm.model.residential_satisfaction.entities.HouseholdPrefsComparatorHouseholdType;
-import at.sume.dm.model.residential_satisfaction.entities.SpatialUnitUdp;
-import at.sume.dm.model.residential_satisfaction.entities.SpatialUnitUdpComparatorSpatialUnitId;
 import at.sume.dm.types.HouseholdType;
 
 /**
+ * Calculate the component of residential satisfaction based on the UDP indicator dimensions diversity and public transport
+ * infrastructure (density has been chosen to be a dynamic model output instead) and household type preferences on these two 
+ * dimensions.  
+ * 
  * @author Alexander Remesch
  *
  */
 public class UDPClassification extends ResidentialSatisfactionComponent {
+	public static class HouseholdPrefs implements Comparable<HouseholdPrefs> {
+		// must be public static in order to be able to use Database.select()/java reflection api
+		public long id;
+		public long scenarioId;
+		public long householdTypeId;
+		public short prefDensity;
+		public short prefDiversity;
+		public short prefEnvAmen;
+		public short prefCosts;
+		public short prefTransportAccess;
+		public short prefLivingSpace;
+		
+		/**
+		 * @return the id
+		 */
+		public long getId() {
+			return id;
+		}
+
+		/**
+		 * @param id the id to set
+		 */
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		/**
+		 * @return the scenarioId
+		 */
+		public long getScenarioId() {
+			return scenarioId;
+		}
+
+		/**
+		 * @param scenarioId the scenarioId to set
+		 */
+		public void setScenarioId(long scenarioId) {
+			this.scenarioId = scenarioId;
+		}
+
+		/**
+		 * @return the householdTypeId
+		 */
+		public long getHouseholdTypeId() {
+			return householdTypeId;
+		}
+
+		/**
+		 * @param householdTypeId the householdTypeId to set
+		 */
+		public void setHouseholdTypeId(long householdTypeId) {
+			this.householdTypeId = householdTypeId;
+		}
+
+		/**
+		 * @return the prefDensity
+		 */
+		public short getPrefDensity() {
+			return prefDensity;
+		}
+
+		/**
+		 * @param prefDensity the prefDensity to set
+		 */
+		public void setPrefDensity(short prefDensity) {
+			this.prefDensity = prefDensity;
+		}
+
+		/**
+		 * @return the prefDiversity
+		 */
+		public short getPrefDiversity() {
+			return prefDiversity;
+		}
+
+		/**
+		 * @param prefDiversity the prefDiversity to set
+		 */
+		public void setPrefDiversity(short prefDiversity) {
+			this.prefDiversity = prefDiversity;
+		}
+
+		/**
+		 * @return the prefEnvAmen
+		 */
+		public short getPrefEnvAmen() {
+			return prefEnvAmen;
+		}
+
+		/**
+		 * @param prefEnvAmen the prefEnvAmen to set
+		 */
+		public void setPrefEnvAmen(short prefEnvAmen) {
+			this.prefEnvAmen = prefEnvAmen;
+		}
+
+		/**
+		 * @return the prefCosts
+		 */
+		public short getPrefCosts() {
+			return prefCosts;
+		}
+
+		/**
+		 * @param prefCosts the prefCosts to set
+		 */
+		public void setPrefCosts(short prefCosts) {
+			this.prefCosts = prefCosts;
+		}
+
+		/**
+		 * @return the prefTransportAccess
+		 */
+		public short getPrefTransportAccess() {
+			return prefTransportAccess;
+		}
+
+		/**
+		 * @param prefTransportAccess the prefTransportAccess to set
+		 */
+		public void setPrefTransportAccess(short prefTransportAccess) {
+			this.prefTransportAccess = prefTransportAccess;
+		}
+
+		/**
+		 * @return the prefLivingSpace
+		 */
+		public short getPrefLivingSpace() {
+			return prefLivingSpace;
+		}
+
+		/**
+		 * @param prefLivingSpace the prefLivingSpace to set
+		 */
+		public void setPrefLivingSpace(short prefLivingSpace) {
+			this.prefLivingSpace = prefLivingSpace;
+		}
+
+		/* (non-Javadoc)
+		 * @see java.lang.Comparable#compareTo(java.lang.Object)
+		 */
+		@Override
+		public int compareTo(HouseholdPrefs o) {
+			if (householdTypeId == o.householdTypeId)
+				return 0;
+			else if (householdTypeId > o.householdTypeId) 
+				return 1;
+			else
+				return -1;
+		}
+	}
+	public static class SpatialUnitUdp implements Comparable<SpatialUnitUdp> {
+		// must be public static in order to be able to use Database.select()/java reflection api
+		public long id;
+		public long spatialUnitId;
+		public int startYear;
+		public int endYear;
+		public short diversityIndicator;
+		public short publicTransportIndicator;
+		
+		/**
+		 * @return the id
+		 */
+		public long getId() {
+			return id;
+		}
+
+		/**
+		 * @param id the id to set
+		 */
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		/**
+		 * @return the spatialUnitId
+		 */
+		public long getSpatialUnitId() {
+			return spatialUnitId;
+		}
+
+		/**
+		 * @param spatialUnitId the spatialUnitId to set
+		 */
+		public void setSpatialUnitId(long spatialUnitId) {
+			this.spatialUnitId = spatialUnitId;
+		}
+
+		/**
+		 * @return the startYear
+		 */
+		public int getStartYear() {
+			return startYear;
+		}
+
+		/**
+		 * @param startYear the startYear to set
+		 */
+		public void setStartYear(int startYear) {
+			this.startYear = startYear;
+		}
+
+		/**
+		 * @return the endYear
+		 */
+		public int getEndYear() {
+			return endYear;
+		}
+
+		/**
+		 * @param endYear the endYear to set
+		 */
+		public void setEndYear(int endYear) {
+			this.endYear = endYear;
+		}
+
+		/**
+		 * @return the diversityIndicator
+		 */
+		public short getDiversityIndicator() {
+			return diversityIndicator;
+		}
+
+		/**
+		 * @param diversityIndicator the diversityIndicator to set
+		 */
+		public void setDiversityIndicator(short diversityIndicator) {
+			this.diversityIndicator = diversityIndicator;
+		}
+
+		/**
+		 * @return the publicTransportIndicator
+		 */
+		public short getPublicTransportIndicator() {
+			return publicTransportIndicator;
+		}
+
+		/**
+		 * @param publicTransportIndicator the publicTransportIndicator to set
+		 */
+		public void setPublicTransportIndicator(short publicTransportIndicator) {
+			this.publicTransportIndicator = publicTransportIndicator;
+		}
+
+		/* (non-Javadoc)
+		 * @see java.lang.Comparable#compareTo(java.lang.Object)
+		 */
+		@Override
+		public int compareTo(SpatialUnitUdp o) {
+			if (spatialUnitId == o.spatialUnitId)
+				return 0;
+			else if (spatialUnitId > o.spatialUnitId) 
+				return 1;
+			else
+				return -1;
+		}
+	}
+
 	private ArrayList<SpatialUnitUdp> spatialUnitUdp;
 	private ArrayList<HouseholdPrefs> householdPrefs;
 	
@@ -47,30 +306,34 @@ public class UDPClassification extends ResidentialSatisfactionComponent {
 	public double calc(HouseholdRow hh, SpatialUnitRow su, int modelYear) {
 		// Lookup the UDP-indicators for the given spatial unit
 		SpatialUnitUdp lookupSpatialUnitUdp = new SpatialUnitUdp();
-		lookupSpatialUnitUdp.spatialUnitId = su.getSpatialUnitId();
-		int posUdp = Collections.binarySearch(spatialUnitUdp, lookupSpatialUnitUdp, new SpatialUnitUdpComparatorSpatialUnitId());
+		lookupSpatialUnitUdp.setSpatialUnitId(su.getSpatialUnitId());
+//		int posUdp = Collections.binarySearch(spatialUnitUdp, lookupSpatialUnitUdp, new SpatialUnitUdpComparatorSpatialUnitId());
+		int posUdp = Collections.binarySearch(spatialUnitUdp, lookupSpatialUnitUdp);
+		assert posUdp >= 0 : "No UDP data found for spatial unit " + su.getSpatialUnitId();
 		lookupSpatialUnitUdp = spatialUnitUdp.get(posUdp);
 		// Get indicator set for the given model year
-		while (lookupSpatialUnitUdp.spatialUnitId == su.getSpatialUnitId()) {
+		while (lookupSpatialUnitUdp.getSpatialUnitId() == su.getSpatialUnitId()) {
 			lookupSpatialUnitUdp = spatialUnitUdp.get(--posUdp);
 		}
 		lookupSpatialUnitUdp = spatialUnitUdp.get(++posUdp);
-		while ((modelYear < lookupSpatialUnitUdp.startYear) || (modelYear > lookupSpatialUnitUdp.endYear)) {
+		while ((modelYear < lookupSpatialUnitUdp.getStartYear()) || (modelYear > lookupSpatialUnitUdp.getEndYear())) {
 			lookupSpatialUnitUdp = spatialUnitUdp.get(++posUdp);
-			if (lookupSpatialUnitUdp.spatialUnitId != su.getSpatialUnitId()) {
+			if (lookupSpatialUnitUdp.getSpatialUnitId() != su.getSpatialUnitId()) {
 				throw new AssertionError("No UDP indicators for spatial unit " + su.getSpatialUnitId() + " in model year " + modelYear + " found");
 			}
 		}
 
 		// Lookup the household preferences for the given household type
 		HouseholdPrefs lookupHouseholdPrefs = new HouseholdPrefs();
-		lookupHouseholdPrefs.householdTypeId = HouseholdType.getId(hh.getHouseholdType());
-		int posHHType = Collections.binarySearch(householdPrefs, lookupHouseholdPrefs, new HouseholdPrefsComparatorHouseholdType());
+		lookupHouseholdPrefs.setHouseholdTypeId(HouseholdType.getId(hh.getHouseholdType()));
+//		int posHHType = Collections.binarySearch(householdPrefs, lookupHouseholdPrefs, new HouseholdPrefsComparatorHouseholdType());
+		int posHHType = Collections.binarySearch(householdPrefs, lookupHouseholdPrefs);		
+		assert posHHType >= 0 : "No household preference data found for hh " + hh.getId() + ", hh type " + hh.getHouseholdType();
 		lookupHouseholdPrefs = householdPrefs.get(posHHType);
 
 		// Calculate the score
-		int hhScore = lookupHouseholdPrefs.prefDiversity * lookupSpatialUnitUdp.diversityIndicator + lookupHouseholdPrefs.prefTransportAccess * lookupSpatialUnitUdp.publicTransportIndicator;
-		int hhScoreMax = lookupHouseholdPrefs.prefDiversity * 4 + lookupHouseholdPrefs.prefTransportAccess * 4;
+		int hhScore = lookupHouseholdPrefs.getPrefDiversity() * lookupSpatialUnitUdp.getDiversityIndicator() + lookupHouseholdPrefs.getPrefTransportAccess() * lookupSpatialUnitUdp.getPublicTransportIndicator();
+		int hhScoreMax = lookupHouseholdPrefs.getPrefDiversity() * 4 + lookupHouseholdPrefs.getPrefTransportAccess() * 4;
 		return (double) hhScore / (double) hhScoreMax;
 	}
 }
