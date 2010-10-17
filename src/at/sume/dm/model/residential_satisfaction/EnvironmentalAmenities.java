4dbabcfbf4c15babbdb97e3598d8ad105ecb5538
Alexander Remesch <alexander.remesch@gmail.com>
2010-10-17 19:06:08 +0000

    Changed todos/comments

--- src/at/sume/dm/model/residential_satisfaction/EnvironmentalAmenities.java
+++ src/at/sume/dm/model/residential_satisfaction/EnvironmentalAmenities.java
@@ -4,6 +4,12 @@
 package at.sume.dm.model.residential_satisfaction;
 
 /**
+ * Calculate the environmental amenities dimension of residential satisfaction based on the share of green area in the
+ * target spatial unit and the respective household preferences
+ *
+ * TODO: the data about green space per spatial unit which is needed for this residential satisfaction component is missing 
+ * from ÖIR (2010-10-16)
+ *  
  * @author Alexander Remesch
  *
  */
