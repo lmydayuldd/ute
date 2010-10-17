5116fbba8a41955fab340241185c8f4d0513925f
Alexander Remesch <alexander.remesch@gmail.com>
2010-10-17 16:14:27 +0000

    RentPerSpatialUnit now again implemented as nested inner class here

--- src/at/sume/dm/model/residential_satisfaction/CostEffectiveness.java
+++ src/at/sume/dm/model/residential_satisfaction/CostEffectiveness.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 import at.sume.dm.Common;
 import at.sume.dm.entities.HouseholdRow;
 import at.sume.dm.entities.SpatialUnitRow;
-import at.sume.dm.model.residential_satisfaction.entities.RentPerSpatialUnit;
 
 /**
  * Calculate the cost-effectiveness dimension of residential satisfaction based on the anticipated costs of a new dwelling with
@@ -24,11 +23,47 @@ import at.sume.dm.model.residential_satisfaction.entities.RentPerSpatialUnit;
  */
 public class CostEffectiveness extends ResidentialSatisfactionComponent {
 	//TODO: find common base class with HouseholdIndicatorsPerSpatialUnit!
+	public static class RentPerSpatialUnit implements Comparable<RentPerSpatialUnit> {
+		// must be public in order to be able to use Database.select()/java reflection api
+		public long spatialUnitId;
+		public double yearlyRentPerSqm;
+		/**
+		 * @return the spatialUnitId
+		 */
+		public long getSpatialUnitId() {
+			return spatialUnitId;
+		}
+		/**
+		 * @param spatialUnitId the spatialUnitId to set
+		 */
+		public void setSpatialUnitId(long spatialUnitId) {
+			this.spatialUnitId = spatialUnitId;
+		}
+		/**
+		 * @return the yearlyPricePerSqm
+		 */
+		public double getYearlyRentPerSqm() {
+			return yearlyRentPerSqm;
+		}
+		/**
+		 * @param yearlyRentPerSqm the yearlyPricePerSqm to set
+		 */
+		public void setYearlyRentPerSqm(double yearlyRentPerSqm) {
+			this.yearlyRentPerSqm = yearlyRentPerSqm;
+		}
+		/* (non-Javadoc)
+		 * @see java.lang.Comparable#compareTo(java.lang.Object)
+		 */
+		@Override
+		public int compareTo(RentPerSpatialUnit arg0) {
+			return ((Long)spatialUnitId).compareTo(arg0.getSpatialUnitId());
+		}
+	}
+
 	private ArrayList<RentPerSpatialUnit> rentPerSpatialUnit;
 	
 	public CostEffectiveness() {
 		try {
-			rentPerSpatialUnit = new ArrayList<RentPerSpatialUnit>();
 			String selectStatement = "SELECT sgt.SpatialUnitId_ZB AS SpatialUnitId, Round(Avg([Preis]) * 12,2) AS YearlyRentPerSqm " +
 			"FROM MA18_Stadtgebietstypen_Zählbezirke  AS sgt INNER JOIN WKO_Mietpreise AS wko ON sgt.SpatialUnitId_AD = wko.SpatialUnitId_AD " +
 			"GROUP BY sgt.SpatialUnitId_ZB;";
