ce69dba3a0c9d8fc5bd2612a6b50d80430f1b1b4
Alexander Remesch <alexander.remesch@gmail.com>
2010-10-22 17:53:33 +0000

    Initial

---
+++ src/at/sume/dm/entities/Dwellings.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package at.sume.dm.entities;
+
+import java.sql.SQLException;
+
+import net.remesch.db.Database;
+import at.sume.db.RecordSet;
+import at.sume.db.RecordSetRow;
+
+/**
+ * @author Alexander Remesch
+ *
+ */
+public class Dwellings extends RecordSet<DwellingRow> {
+
+	public Dwellings(Database db) throws SQLException, InstantiationException, IllegalAccessException {
+		setDb(db);
+		rowList = db.select(DwellingRow.class, selectStatement());
+	}
+	
+	/* (non-Javadoc)
+	 * @see at.sume.db.RecordSet#tablename()
+	 */
+	@Override
+	public String tablename() {
+		return "_DM_Dwellings";
+	}
+
+	/* (non-Javadoc)
+	 * @see at.sume.db.RecordSet#selectStatement()
+	 */
+	@Override
+	public String selectStatement() {
+		return "select dwellingId, spatialunitId, dwellingSize, dwellingCosts, livingSpaceGroup6Id, costOfResidenceGroupId, constructionPeriod7Id, taken from _DM_Dwellings " +
+		"order by dwellingId";
+	}
+
+	/* (non-Javadoc)
+	 * @see at.sume.db.RecordSet#primaryKeyFieldnames()
+	 */
+	@Override
+	public String[] primaryKeyFieldnames() {
+		throw new AssertionError("Dwellings.primaryKeyFieldnames is depreceated");
+	}
+
+	/* (non-Javadoc)
+	 * @see at.sume.db.RecordSet#fieldnames()
+	 */
+	@Override
+	public String[] fieldnames() {
+		throw new AssertionError("Dwellings.fieldnames is depreceated");
+	}
+
+	/* (non-Javadoc)
+	 * @see at.sume.db.RecordSet#createRecordSetRow()
+	 */
+	@Override
+	public DwellingRow createRecordSetRow() {
+		throw new AssertionError("Dwellings.createRecordSetRow is depreceated");
+	}
+
+	/**
+	 * Link all households in the collection to their corresponding spatial unit according to the spatialunit-id
+	 * @param spatialunits Collection of spatial units
+	 */
+	public void linkSpatialUnits(SpatialUnits spatialunits) {
+		for (RecordSetRow<Dwellings> row : rowList) {
+			DwellingRow hh = (DwellingRow) row;
+			hh.setSpatialunit(spatialunits.lookup(hh.getSpatialunitId()));
+		}
+	}
+}
