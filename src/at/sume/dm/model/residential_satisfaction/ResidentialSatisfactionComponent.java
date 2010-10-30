31e394a05ce17e7b02b9b7db19f790aa2feabd50
Alexander Remesch <alexander.remesch@gmail.com>
2010-10-30 08:51:21 +0000

    Recognize dwelling in residential satisfaction calculation

--- src/at/sume/dm/model/residential_satisfaction/ResidentialSatisfactionComponent.java
+++ src/at/sume/dm/model/residential_satisfaction/ResidentialSatisfactionComponent.java
@@ -3,6 +3,7 @@
  */
 package at.sume.dm.model.residential_satisfaction;
 
+import at.sume.dm.entities.DwellingRow;
 import at.sume.dm.entities.HouseholdRow;
 import at.sume.dm.entities.SpatialUnitRow;
 
@@ -12,7 +13,9 @@ import at.sume.dm.entities.SpatialUnitRow;
  */
 public class ResidentialSatisfactionComponent {
 	/**
-	 * Calculate residential satisfaction for the household in the given spatial unit
+	 * Calculate residential satisfaction for the household in the given spatial unit (without
+	 * considering dwelling characteristics)
+	 * 
 	 * @param hh
 	 * @param su
 	 * @return Residential satisfaction in thousandth part
@@ -22,11 +25,23 @@ public class ResidentialSatisfactionComponent {
 	}
 	
 	/**
-	 * Calculate residential satisfaction for the household in its current spatial unit
+	 * Calculate residential satisfaction for the household in its current spatial unit & dwelling
 	 * @param hh
 	 * @return Residential satisfaction in thousandth part
 	 */
 	public final long calc(HouseholdRow hh, int modelYear) {
 		return calc(hh, hh.getSpatialunit(), modelYear);
+	}
+	
+	/**
+	 * Calculate residential satisfcation for the household for the given dwelling
+	 * 
+	 * @param hh
+	 * @param dwelling
+	 * @param modelYear
+	 * @return Residential satisfaction in thousandth part
+	 */
+	public long calc(HouseholdRow hh, DwellingRow dwelling, int modelYear) {
+		return calc(hh, dwelling.getSpatialunit(), modelYear);
 	}
 }
