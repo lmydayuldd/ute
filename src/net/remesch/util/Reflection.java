8fbf92ffc609c6f8999d5c7d4a54d0123cd10e45
Alexander Remesch <alexander.remesch@gmail.com>
2010-11-24 16:43:15 +0000

    optimized field retrieval, but SQLException on public fields

--- src/net/remesch/util/Reflection.java
+++ src/net/remesch/util/Reflection.java
@@ -6,6 +6,9 @@ package net.remesch.util;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
+import net.remesch.db.schema.DatabaseField;
+import net.remesch.db.schema.DatabaseFieldMap;
+
 /**
  * @author Alexander Remesch
  *
@@ -18,7 +21,7 @@ public class Reflection {
 	 * @param c
 	 * @return
 	 */
-	public static <T> Field[] getFields(Class<T> c) {
+	public static <T> Field[] getFieldNames(Class<T> c) {
 		ArrayList<Field> result = new ArrayList<Field>();
 		for (Field field : c.getFields()) {
 			result.add(field);
@@ -30,5 +33,53 @@ public class Reflection {
 		}
 		Field fields[] = new Field[result.size()];
 		return result.toArray(fields);
+	}
+	/**
+	 * Get an ArrayList of all field-related information needed to load a database table into a class
+	 * @param <T>
+	 * @param c
+	 * @return
+	 */
+	public static <T> ArrayList<DatabaseFieldMap> getFields(Class<T> c) {
+		ArrayList<DatabaseFieldMap> result = new ArrayList<DatabaseFieldMap>();
+		for (int i = 0; i != 2; i++) {
+			Field fields[];
+			switch(i) {
+			case 0:
+				// all public & private fields in classes
+				fields = c.getDeclaredFields();
+				break;
+			case 1:
+				// all public fields in classes & superclasses
+				fields = c.getFields();
+				break;
+			default:
+				throw new AssertionError("i = " + i);
+			}
+			for (Field field : fields) {
+				if (field.isAnnotationPresent(net.remesch.db.schema.Ignore.class))
+					continue;
+				if (result.contains(field))
+					continue;
+				DatabaseFieldMap fieldMap = new DatabaseFieldMap();
+				fieldMap.setClassFieldName(field.getName());
+				fieldMap.setFieldType(field.getType().getName());
+				fieldMap.setField(field);
+				if (field.isAnnotationPresent(net.remesch.db.schema.DatabaseField.class)) {
+					DatabaseField dbf = field.getAnnotation(net.remesch.db.schema.DatabaseField.class);
+					fieldMap.setDbFieldName(dbf.fieldName());
+				} else {
+					fieldMap.setDbFieldName(fieldMap.getClassFieldName());
+				}
+				if (!field.isAccessible()) {
+					field.setAccessible(true);
+					fieldMap.setInacessible(true);
+				} else {
+					fieldMap.setInacessible(false);
+				}
+				result.add(fieldMap);
+			}
+		}
+		return result;
 	}
 }
