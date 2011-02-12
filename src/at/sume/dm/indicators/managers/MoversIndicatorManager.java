7991cd9936dc8712139156a9167c3627b5ab048c
Alexander Remesch <alexander.remesch@gmail.com>
2011-02-12 18:53:50 +0000

    Changed from enum to class

--- src/at/sume/dm/indicators/managers/MoversIndicatorManager.java
+++ src/at/sume/dm/indicators/managers/MoversIndicatorManager.java
@@ -3,6 +3,8 @@
  */
 package at.sume.dm.indicators.managers;
 
+import java.util.ArrayList;
+
 import at.sume.dm.entities.HouseholdRow;
 import at.sume.dm.indicators.MoversIndicatorsPerSpatialUnit;
 import at.sume.dm.indicators.base.Indicator;
@@ -11,31 +13,33 @@ import at.sume.dm.indicators.base.Indicator;
  * @author Alexander Remesch
  *
  */
-public enum MoversIndicatorManager {
-	INDICATORS_PER_SPATIALUNIT("Indicators per spatial unit", new MoversIndicatorsPerSpatialUnit());
+public class MoversIndicatorManager {
+	public static final MoversIndicatorManager INDICATORS_PER_SPATIALUNIT = new MoversIndicatorManager("Indicators per spatial unit", new MoversIndicatorsPerSpatialUnit());
 
-	private String label;
+	private static ArrayList<MoversIndicatorManager> values; 
+
+//	private String label;
 	private Indicator<HouseholdRow> indicator;
 
 	MoversIndicatorManager(String label, Indicator<HouseholdRow> indicator) {
-		this.label = label;
+//		this.label = label;
 		this.indicator = indicator;
 	}
 	
 	public static void addHousehold(HouseholdRow household) {
-		for (MoversIndicatorManager indicatorManager : values()) {
+		for (MoversIndicatorManager indicatorManager : values) {
 			indicatorManager.indicator.add(household);
 		}
 	}
 	
 	public static void removeHousehold(HouseholdRow household) {
-		for (MoversIndicatorManager indicatorManager : values()) {
+		for (MoversIndicatorManager indicatorManager : values) {
 			indicatorManager.indicator.remove(household);
 		}
 	}
 	
 	public static void resetIndicators() {
-		for (MoversIndicatorManager indicatorManager : values()) {
+		for (MoversIndicatorManager indicatorManager : values) {
 			indicatorManager.indicator.clear();
 		}
 	}
