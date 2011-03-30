46926331eaaa02e45db568ac9c9cc3c608a3d2e4
Alexander Remesch <alexander.remesch@gmail.com>
2011-03-30 08:56:20 +0000

    Added random number generator for triangular distribution

--- src/net/remesch/util/Random.java
+++ src/net/remesch/util/Random.java
@@ -25,4 +25,24 @@ public class Random extends java.util.Random {
 		} while ((result > range) || (result < -range));
 		return result;
 	}
+	/**
+	 * Returns the next pseudorandom, triangular distributed double value, ranging from min to max with mode as the most frequent value. 
+	 * @param min Minimum return value
+	 * @param max Maximum return value
+	 * @param mode Most frequent return value
+	 * @return
+	 */
+	public double triangular(double min, double max, double mode) {
+		double bound = (mode - min) / (max - min);
+		double r = nextDouble();
+		double result = 0;
+		if (r < bound) {
+			result = min + Math.sqrt(r * (max - min) * (mode - min));
+		} else {
+			result = max - Math.sqrt((1 - r) * (max - min) * (max - mode));
+		}
+		assert min <= result : "Result " + result + " < min " + min;
+		assert result <= max : "Result " + result + " > max " + max;
+		return result;
+	}
 }
